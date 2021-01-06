c4ff91b2d900931ee669eda516b877e4bf5c1e52
李韻晟 <dreak603@gmail.com>
2021-01-06 23:58:54 +0800

    chore: update config

--- config.ts
+++ config.ts
@@ -81,7 +81,7 @@ const configs = {
       password: process.env.DBPASSWORD || '123',
       database: process.env.DBDATABASE || 'onepiece',
       schema: process.env.DBSCHEMA || 'public',
-      tripTable: process.env.DBRATETABLE || 'trip',
+      table: process.env.DBRATETABLE || 'trip',
     },
 
     REDIS_URL: process.env.REDIS_URL || "redis://127.0.0.1:6381",
