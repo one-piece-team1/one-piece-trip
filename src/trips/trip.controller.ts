795b27d900d533587ce9252ca00706a5de83e5f9
李韻晟 <dreak603@gmail.com>
2021-01-15 01:11:34 +0800

    feat: adding role guards

--- src/trips/trip.controller.ts
+++ src/trips/trip.controller.ts
@@ -1,19 +1,28 @@
 import {
   Controller,
   Get,
-  Post,
-  UsePipes,
-  ValidationPipe,
+  Logger,
+  SetMetadata,
+  UseGuards,
 } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { CurrentUser } from './get-user.decorator';
 import { TripService } from './trip.service';
+import * as IUser from '../interfaces';
+import * as Euser from '../enums';
+import { RoleGuard } from 'guards/local-guard';
 
 @Controller('trips')
 export class TripController {
+  private readonly logger: Logger = new Logger('TripController');
+
   constructor(private readonly tripService: TripService) {}
 
-  @Get()
-  @UsePipes(ValidationPipe)
-  getRequest(): Promise<string> {
+  @Get('/usertest')
+  @SetMetadata('roles', [Euser.EUserRole.USER])
+  @UseGuards(AuthGuard(['jwt']), RoleGuard)
+  getRequest(@CurrentUser() user: IUser.UserInfo): Promise<string> {
+    this.logger.log(user);
     return this.tripService.getRequest();
   }
 }
